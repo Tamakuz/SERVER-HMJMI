09503b49e77cb74f3ffee4ed516f9fc3e1662d42
Tamakuz <jovangithub@gmail.com>
2023-05-10 20:51:10 +0700

    new controller get image

--- services/image/index.js
+++ services/image/index.js
@@ -1,22 +1,18 @@
 import admin from "firebase-admin";
 
 const getImage = async (req, res, next) => {
-  const { filename } = req.params;
-  const folderNames = ["collages", "lectures", "works"];
-  for (let i = 0; i < folderNames.length; i++) {
-    const folderName = folderNames[i];
-    const path = `${folderName}/${filename}`;
-    try {
-      const storageRef = admin.storage().bucket().file(path);
-      const url = await storageRef.getSignedUrl({
-        action: "read",
-        expires: Date.now() + 86400 * 1000, // Set expiry date of the URL
-      });
-      res.status(200).send(url);
-      return;
-    } catch (error) {
-      console.error(error);
-    }
+  const { filename, folderName } = req.params;
+  const path = `${folderName}/${filename}`;
+  try {
+    const storageRef = admin.storage().bucket().file(path);
+    const url = await storageRef.getSignedUrl({
+      action: "read",
+      expires: Date.now() + 86400 * 1000, // Set expiry date of the URL
+    });
+    res.status(200).send(url);
+    return;
+  } catch (error) {
+    console.error(error);
   }
 };
 
