ef5b4a3739a1c6b198a4f2aa832a996316336e27
Tamakuz <jovangithub@gmail.com>
2023-05-10 20:53:07 +0700

    new controller get image

--- services/image/index.js
+++ services/image/index.js
@@ -1,8 +1,8 @@
 import admin from "firebase-admin";
 
 const getImage = async (req, res, next) => {
-  const { filename, folderName } = req.params;
-  const path = `${folderName}/${filename}`;
+  const { filename, foldername } = req.params;
+  const path = `${foldername}/${filename}`;
   try {
     const storageRef = admin.storage().bucket().file(path);
     const url = await storageRef.getSignedUrl({
