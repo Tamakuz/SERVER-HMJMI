a4181e5735b896d8e126c90201438fc4a043f53a
Tamakuz <jovangithub@gmail.com>
2023-05-11 18:37:06 +0700

    optimasio login

--- services/login/login.js
+++ services/login/login.js
@@ -6,53 +6,52 @@ import config from "../../config/index.js";
 import createError from "../../utils/error.js";
 import responseSuccess from "../../utils/responseSuccess.js";
 
-const refreshTokenSecret = config.refreshTokenSecret;
-
-const authenticateUser = async (username, password, model) => {
-  const user = await model.findOne({ username: username });
-
-  if (!user) {
-    return createError(400, "Masukan username yang terdaftar");
-  }
-
-  const match = await bcrypt.compare(password, user.password);
-
-  if (!match) {
-    return createError(400, "Wrong Password");
-  }
-
-  const userId = user._id;
-  const userRole = user.role;
-  const refreshToken = jwt.sign(
-    { id: userId, role: userRole },
-    refreshTokenSecret,
-    {
-      expiresIn: "1d",
-    }
-  );
-
-  await user.updateOne({ refresh_token: refreshToken });
-  return refreshToken;
-};
-
 const login = async (req, res, next) => {
-  const { username, password } = req.body;
-
   try {
-    const response =
-      (await authenticateUser(username, password, Lecture)) ||
-      (await authenticateUser(username, password, Collage));
-      console.log(response);
+    const { username, password } = req.body;
+    const { accessTokenSecret, refreshTokenSecret } = config;
 
-      if(response.status === 400){
-        next(createError(response.status, response.message))
-      }else{
-        responseSuccess(res, {refresToken : response})
+    const lecture = await Lecture.findOne({ username: username });
+    const collage = await Collage.findOne({ username: username });
+
+    if (lecture) {
+      const matchLecture = await bcrypt.compare(password, lecture.password);
+
+      if (!matchLecture) {
+        next(createError(400, "Wrong Password"));
       }
-    
+
+      const lectureId = lecture._id;
+      const lectureRole = lecture.role;
+      const refreshToken = jwt.sign({ id: lectureId, role: lectureRole }, refreshTokenSecret, {
+        expiresIn: "1d",
+      });
+      await lecture.updateOne({ refresh_token: refreshToken });
+      responseSuccess(res, { refreshToken });
+    }
+
+    if (collage) {
+      const matchCollage = await bcrypt.compare(password, collage.password);
+
+      if (!matchCollage) {
+        next(createError(400, "Wrong Password"));
+      }
+
+      const collageId = collage._id;
+      const collageRole = collage.role;
+      const refreshToken = jwt.sign({ id: collageId, role: collageRole }, refreshTokenSecret, {
+        expiresIn: "1d",
+      });
+      await collage.updateOne({ refresh_token: refreshToken });
+      responseSuccess(res, { refreshToken });
+    }
+
+    if (!lecture || !collage) {
+      next(createError(400, "Masukan username yang terdaftar"));
+    }
   } catch (error) {
     console.log(error);
-    res.status(500).send("Server Error");
+    next(createError(500, "Server Error"));
   }
 };
 
