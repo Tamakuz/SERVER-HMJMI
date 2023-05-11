4c633a52dc1b508e8ebeea77e0b4e05c5e3eaffa
Tamakuz <jovangithub@gmail.com>
2023-05-11 18:50:18 +0700

    optimasi api

--- services/collages/profiles/getSingleCollage.js
+++ services/collages/profiles/getSingleCollage.js
@@ -1,5 +1,5 @@
 import Collage from "../../../models/collageModel.js";
-import Work from "../../../models/workModel.js"
+import Work from "../../../models/wo"
 import createError from "../../../utils/error.js";
 import responseSuccess from "../../../utils/responseSuccess.js";
 
@@ -8,20 +8,13 @@ const getSingleCollage = async (req, res, next) => {
 
   let collage;
   try {
-    collage = await Collage.findById(id).lean();
+    collage = await Collage.findById(id).populate("workcollage");
   } catch (error) {
     return next(createError(500, "Server Error"));
   }
 
   if (!collage) {
     return next(createError(404, "Data tidak dapat ditemukan"));
-  }
-
-  // populate "workcollage" field manually
-  try {
-    collage.workcollage = await Work.find({ collageId: id });
-  } catch (error) {
-    return next(createError(500, "Server Error"));
   }
 
   responseSuccess(res, collage);
