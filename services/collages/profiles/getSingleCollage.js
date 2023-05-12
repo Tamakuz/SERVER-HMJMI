cb954bb82eb68c2cab5b2327c7e1c6c5b58819d3
Tamakuz <jovangithub@gmail.com>
2023-05-13 00:35:41 +0700

    adding cache feature

--- services/collages/profiles/getSingleCollage.js
+++ services/collages/profiles/getSingleCollage.js
@@ -1,22 +1,32 @@
 import Collage from "../../../models/collageModel.js";
 import createError from "../../../utils/error.js";
 import responseSuccess from "../../../utils/responseSuccess.js";
+import cache from "memory-cache"
 
 const getSingleCollage = async (req, res, next) => {
   const { id } = req.params;
 
-  let collage;
-  try {
-    collage = await Collage.findById(id).populate("workcollage");
-  } catch (error) {
-    return next(createError(500, "Server Error"));
-  }
+  const key = '__express__' + req.originalUrl || req.url;
+  const cachedResponse = cache.get(key);
+  if (cachedResponse) {
+    responseSuccess(res, cachedResponse);
+    return;
+  } else {
+    let collage;
+    try {
+      collage = await Collage.findById(id).populate("workcollage");
+    } catch (error) {
+      return next(createError(500, "Server Error"));
+    }
 
-  if (!collage) {
-    return next(createError(404, "Data tidak dapat ditemukan"));
-  }
+    if (!collage) {
+      return next(createError(404, "Data tidak dapat ditemukan"));
+    }
 
-  responseSuccess(res, collage);
+    cache.put(key, collage, duration * 1000); // Menyimpan data pada cache selama durasi yang ditentukan (dalam detik)
+    responseSuccess(res, collage);
+  }
 };
 
+const duration = 60; // Durasi penyimpanan data pada cache dalam detik
 export default getSingleCollage;
