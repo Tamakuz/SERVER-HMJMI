0fd09a909924b9a46258bd5c41a788d9bf557f66
Tamakuz <jovangithub@gmail.com>
2023-07-12 20:18:34 +0700

    add image tecnologi

--- src/services/collages/works/createWork.js
+++ src/services/collages/works/createWork.js
@@ -49,13 +49,9 @@ const createWork = async (req, res, next) => {
             { headers: formData.getHeaders() }
           );
 
-          if (response.data && response.data.success) {
-            work.thumbnail = response.data.data.url;
-            work.delete_url = response.data.data.delete_url;
-            await work.save();
-          } else {
-            throw new Error("Upload gagal!");
-          }
+          work.thumbnail = response.data.data.url;
+          work.delete_url = response.data.data.delete_url;
+          await work.save();
         } catch (error) {
           console.log(error.response.data.error);
           return next(createError(400, "Upload gagal!"));
